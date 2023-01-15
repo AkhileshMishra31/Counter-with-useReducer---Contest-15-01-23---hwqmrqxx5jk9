4546806873cd2d9ccea2ad39ad90cf71ab746861
AkhileshMishra31 <89763916+AkhileshMishra31@users.noreply.github.com>
2023-01-15 21:46:25 +0530

    new

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,11 @@
 const counterReducer = (state,action) =>{
-
+   if(action.type=='INCREMENT'){
+     return state+1; 
+   }
+  if(action.type=='DECREMENT'){
+    return state-1;
+  }
+  return state;
 }
 
 export {counterReducer}
